89764849e38fdbcb4aa0357faea935845d9c73b7
DennisLi755 <djl6347@g.rit.edu>
2024-11-05 10:58:01 -0500

    details page is function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import { Movie } from "./Movie";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [clickedMovie, setClickedMovie] = useState({});
+  const [showDetails, setShowDetails] = useState(false);
 
   const bucketName = "team-7-tmdb-movie-data-a1b2c3";
   const fileName = "movie_data.json";
@@ -16,10 +19,23 @@ function App() {
       .catch((error) => console.log("Error fetching movie data:", error));
   }, []);
 
+  const clickMovie = (id, title, image, genres, synopsis) => {
+    setClickedMovie({"id": id, "title": title, "image": image, "genres": genres, "synopsis": synopsis});
+    setShowDetails(true);
+  }
+
+  const backToHome = (e) => {
+    e.preventDefault();
+    setShowDetails(false);
+  }
+
   return (
     <div className="container">
-      <h1>Movie Recommendation System</h1>
+      <h1 onClick={(e) => backToHome(e)}><a href="/">Movie Recommendation System</a></h1>
 
+      {showDetails ? 
+      <Movie id={clickedMovie.id} title={clickedMovie.title} image={clickedMovie.image} genres={clickedMovie.genres} synopsis={clickedMovie.synopsis} />
+      :
       <div className="movie-list">
         {movies.map((movie) => (
           <div key={movie.id} className="movie-card">
@@ -27,11 +43,13 @@ function App() {
               src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
               alt={movie.title}
               className="movie-poster"
+              onClick={() => clickMovie(movie.id, movie.title, movie.poster_path, movie.genre_ids, movie.overview)}  
             />
             <h2 className="movie-title">{movie.title}</h2>
           </div>
         ))}
       </div>
+      }
     </div>
   );
 }
