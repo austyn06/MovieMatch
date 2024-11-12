7cae6436618a035642573757093bcbc7237bcf21
cmb6650 <90795816+cmb6650@users.noreply.github.com>
2024-11-12 18:31:53 -0500

    added links for local development, amplify not working so commented out

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_fNBa2g3Gs',
+    userPoolWebClientId: '10gn06qn308vmrse43j6obkrts',
+  },
+});
 
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const bucketName = "team-7-tmdb-movie-data-a1b2c3";
-  const fileName = "movie_data.json";
-  const s3Url = `https://${bucketName}.s3.amazonaws.com/${fileName}`;
+  // const bucketName = "team-7-tmdb-movie-data-a1b2c3";
+  // const fileName = "movie_data.json";
+  // const s3Url = `https://${bucketName}.s3.amazonaws.com/${fileName}`;
+
+  // Use TMDb API URL for local development
+  const apiKey = "805e414f9ae84a75b0cf3d95476e199a"; // Avoid committing this
+  const tmdbUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
 
   useEffect(() => {
-    fetch(s3Url)
+    fetch(tmdbUrl) //change this to s3Url
       .then((response) => response.json())
       .then((data) => setMovies(data.results))
       .catch((error) => console.log("Error fetching movie data:", error));
