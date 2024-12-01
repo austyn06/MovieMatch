8351f25f9f98777120a78c87f4b7e68b22eedb97
cmb6650 <90795816+cmb6650@users.noreply.github.com>
2024-12-01 13:46:19 -0500

    got login page set up, not working yet

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,42 +1,37 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-// import { Amplify} from 'aws-amplify';
-// import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
-// import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import "./login.css";
-// import awsConfig from '../../aws-exports';
+import awsConfig from '../../aws-exports';
 
-// Amplify.configure(awsConfig);
+Amplify.configure(awsConfig);
 
 export const Login = () => {
-  // const { signOut, user } = useAuthenticator(context => [context.user]);
   const navigate = useNavigate();
 
   return (
-    // <Authenticator>
-    //     {({ signOut, user }) => (
-    //         <>
-    //             <NavBar />
-    //             <div className="login-container">
-    //                 {user ? (
-    //                     <div>
-    //                         <h3>Welcome, {user.username}</h3>
-    //                         <button onClick={signOut}>Sign Out</button>
-    //                     </div>
-    //                 ) : (
-    //                     <div>
-    //                         <h3>Please sign in to access your account</h3>
-    //                         <Authenticator.SignIn />
-    //                     </div>
-    //                 )}
-    //             </div>
-    //         </>
-    //     )}
-    // </Authenticator>
-    <div>
-      <div className="center">
-        <h1>Login Page</h1>
-      </div>
+    <div className="login-page">
+      <Authenticator>
+        {({ signOut, user }) => (
+          <div className="login-container">
+            {user ? (
+              <div>
+                <h3>Welcome, {user.username}</h3>
+                <button onClick={signOut}>Sign Out</button>
+              </div>
+            ) : (
+              <div>
+                <h3>Please sign in to access your account</h3>
+              </div>
+            )}
+            <div className="center">
+              <h1>Login Page</h1>
+            </div>
+          </div>
+        )}
+      </Authenticator>
     </div>
   );
 };
