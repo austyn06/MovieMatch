c754ba620fd65fe3802be05ada668913b2fa8fa5
austyn06 <austynwright06@gmail.com>
2026-01-03 23:15:28 -0600

    Clean up terraform folder and update gitignore

--- terraform/lambda-auth.js
+++
@@ -1,9 +0,0 @@
-exports.handler = (event, context, callback) => {
-    // Automatically confirm the user
-    event.response.autoConfirmUser = true;
-    // Set email as verified
-    event.response.autoVerifyEmail = true;
-    
-    // Return to Amazon Cognito
-    callback(null, event);
-};
